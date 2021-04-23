19ea43213bb4bc88530a6d569b026316cf8d426f
RachaelMSimmonds <rms1252@rit.edu>
2021-04-23 12:17:12 -0400

    Starter Files for project added but not working yet

---
+++ IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.simmonds.rachael.islandwrapsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.simmonds.rachael.islandwrapsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       // setSupportActionBar(binding.toolbar)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        setupBottomNavMenu(navController)
+        setupSideNavigationMenu(navController)
+        setActionBar(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        binding.bottomNav?.let {
+            // using ? because in landscape it won't exist
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setupSideNavigationMenu(navController: NavController) {
+        binding.bottomNav?.let {
+            // using ? because in landscape it won't exist
+            NavigationUI.setupWithNavController(it, navController)
+        }
+    }
+
+    private fun setActionBar(navController: NavController) {
+        // so that the hamburger changes to/from correctly
+        NavigationUI.setupActionBarWithNavController(this, navController, binding.drawerLayout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // could be a class variable/property
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        val navigated = NavigationUI.onNavDestinationSelected(item, navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.nav_host_fragment), binding.drawerLayout) // so the hamburger/back work correctly
+    }
+
+}
