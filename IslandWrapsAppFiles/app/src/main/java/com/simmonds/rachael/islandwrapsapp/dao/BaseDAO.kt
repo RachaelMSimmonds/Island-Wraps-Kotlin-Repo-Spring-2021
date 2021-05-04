c34448d9697172a6b153129790e06f67e328c7e5
alexisRivaldo <axr7776@g.rit.edu>
2021-05-04 14:07:14 -0400

    continued work on Intent for menu

--- IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/dao/BaseDAO.kt
+++ IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/dao/BaseDAO.kt
@@ -1,20 +1,20 @@
 package com.french.bryan.roomdemo.dao
 
-import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
-import androidx.room.Update
-
-@Dao
-interface BaseDao<in T> {
-
-    @Insert(onConflict = REPLACE)
-    suspend fun insert(t: T)
-
-    @Update
-    suspend fun update(t: T)
-
-    @Delete
-    suspend fun delete(t: T)
-}
+//import androidx.room.Dao
+//import androidx.room.Delete
+//import androidx.room.Insert
+//import androidx.room.OnConflictStrategy.REPLACE
+//import androidx.room.Update
+//
+//@Dao
+//interface BaseDao<in T> {
+//
+//    @Insert(onConflict = REPLACE)
+//    suspend fun insert(t: T)
+//
+//    @Update
+//    suspend fun update(t: T)
+//
+//    @Delete
+//    suspend fun delete(t: T)
+//}
--- IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/viewmodels/AllMenuItemsViewModel.kt
+++ IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/viewmodels/AllMenuItemsViewModel.kt
@@ -1,36 +1,36 @@
 package com.simmonds.rachael.islandwrapsapp
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import com.french.bryan.roomdemo.InvoiceRepository
-import com.simmonds.rachael.islandwrapsapp.ItemRepository
-import com.simmonds.rachael.islandwrapsapp.entities.Invoice
-
-class AllMenuItemsViewModel : AndroidViewModel {
-    private var mItemRepository : ItemRepository? = null
-    private var customerId: Long? = null
-
-
-    constructor(application: Application, custId: Long) : super(application) {
-        customerId = custId
-        mItemRepository = ItemRepository(getApplication(), customerId!!)
-    }
-
-
-    fun getAllItems(): LiveData<List<Invoice>> {
-        return mItemRepository!!.mAllInvoices
-    }
-
-
-    fun insertInvoice(invoice: Invoice) {
-        mItemRepository!!.insertInvoice(invoice)
-    }
-
-    //other wrapper methods for inserting/deleting, etc.
-//swipe to delete
-    fun deleteInvoice(invoice: Invoice) {
-        mItemRepository!!.deleteInvoice(invoice)
-    }
-//***
-}
+//import android.app.Application
+//import androidx.lifecycle.AndroidViewModel
+//import androidx.lifecycle.LiveData
+//import com.french.bryan.roomdemo.InvoiceRepository
+//import com.simmonds.rachael.islandwrapsapp.ItemRepository
+//import com.simmonds.rachael.islandwrapsapp.entities.Invoice
+//
+//class AllMenuItemsViewModel : AndroidViewModel {
+//    private var mItemRepository : ItemRepository? = null
+//    private var customerId: Long? = null
+//
+//
+//    constructor(application: Application, custId: Long) : super(application) {
+//        customerId = custId
+//        mItemRepository = ItemRepository(getApplication(), customerId!!)
+//    }
+//
+//
+//    fun getAllItems(): LiveData<List<Invoice>> {
+//        return mItemRepository!!.mAllInvoices
+//    }
+//
+//
+//    fun insertInvoice(invoice: Invoice) {
+//        mItemRepository!!.insertInvoice(invoice)
+//    }
+//
+//    //other wrapper methods for inserting/deleting, etc.
+////swipe to delete
+//    fun deleteInvoice(invoice: Invoice) {
+//        mItemRepository!!.deleteInvoice(invoice)
+//    }
+////***
+//}
