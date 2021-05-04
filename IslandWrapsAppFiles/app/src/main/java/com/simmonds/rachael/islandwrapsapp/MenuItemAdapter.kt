1097e9edad5cf1e9b412328f8c9a8d93d13fe08e
alexisRivaldo <axr7776@g.rit.edu>
2021-05-04 13:22:06 -0400

    starting to try something different for menu

--- IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/MenuItemAdapter.kt
+++ IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/MenuItemAdapter.kt
@@ -7,37 +7,36 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.simmonds.rachael.islandwrapsapp.databinding.ActivityMainBinding
-import com.simmonds.rachael.islandwrapsapp.entities.Invoice
 
-class MenuItemAdapter(private var fragment: WrapFragment, private var mData: List<Items>)
-    : androidx.recyclerview.widget.RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>() {
+//class MenuItemAdapter(private var fragment: WrapFragment, private var mData: List<Items>)
+   // : androidx.recyclerview.widget.RecyclerView.Adapter<androidx.recyclerview.widget.RecyclerView.ViewHolder>() {
 
-    private var _binding: FragmentMenuItemAdapterBinding? = null
-    private val binding get() = _binding!!
-
-    fun addAll(invoices: List<Items>) {
-        mData = invoices
-        notifyDataSetChanged()
-    }
-
-    inner class ViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView)  {
-
-        var itemDescription: TextView
-        var itemName: TextView
-
-        init {
-            itemDescription = itemView.itemDescription
-            itemName = itemView.itemName
-        }
-    }
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v: View
-        val vh: androidx.recyclerview.widget.RecyclerView.ViewHolder
-            v = LayoutInflater.from(viewGroup.context)
-                    .inflate(R.layout.fragment_wrap, viewGroup, false)
-            vh = ViewHolder(v)
-
-        return vh
-    }
-}
+//    private var _binding: FragmentMenuItemAdapterBinding? = null
+//    private val binding get() = _binding!!
+//
+//    fun addAll(invoices: List<Items>) {
+//        mData = invoices
+//        notifyDataSetChanged()
+//    }
+//
+//    inner class ViewHolder(itemView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(itemView)  {
+//
+//        var itemDescription: TextView
+//        var itemName: TextView
+//
+//        init {
+//            itemDescription = itemView.itemDescription
+//            itemName = itemView.itemName
+//        }
+//    }
+//
+//    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+//        val v: View
+//        val vh: androidx.recyclerview.widget.RecyclerView.ViewHolder
+//            v = LayoutInflater.from(viewGroup.context)
+//                    .inflate(R.layout.fragment_wrap, viewGroup, false)
+//            vh = ViewHolder(v)
+//
+//        return vh
+//    }
+//}
--- IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/WrapFragment.kt
+++ IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/WrapFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.simmonds.rachael.islandwrapsapp.viewmodels.AllMenuItemsViewModel
+//import com.simmonds.rachael.islandwrapsapp.viewmodels.AllMenuItemsViewModel
 import com.simmonds.rachael.islandwrapsapp.databinding.FragmentWrapBinding
-import com.simmonds.rachael.islandwrapsapp.entities.Items
+//import com.simmonds.rachael.islandwrapsapp.entities.Items
 
 //private const val CUSTOMER_ID = "customerId"
 private const val ITEM_NAME = "itemName"
@@ -17,7 +17,7 @@ class WrapFragment : Fragment() {
     private var _binding: FragmentWrapBinding? = null
     private val binding get() = _binding!!
 
-    var mAdapter: MenuItemAdapter? = null
+    //var mAdapter: MenuItemAdapter? = null
     private var recyclerView : androidx.recyclerview.widget.RecyclerView? = null
     private var layoutManager: androidx.recyclerview.widget.RecyclerView.LayoutManager? = null
 
@@ -39,23 +39,23 @@ class WrapFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_wrap, container, false)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            //customerId = it.getLong(CUSTOMER_ID)
-            itemName = it.getString(ITEM_NAME,"")
-        }
-        menuItemViewModel = AllMenuItemsViewModel(activity?.application!!, itemName!!)
-
-        mAdapter = MenuItemAdapter(this,
-                mutableListOf<Invoice>())
-
-
-        menuItemViewModel!!.getAllItems().observe(this, Observer {
-            mAdapter!!.addAll(it ?: emptyList())
-        })
-
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            //customerId = it.getLong(CUSTOMER_ID)
+//            itemName = it.getString(ITEM_NAME,"")
+//        }
+//        menuItemViewModel = AllMenuItemsViewModel(activity?.application!!, itemName!!)
+//
+//        mAdapter = MenuItemAdapter(this,
+//                mutableListOf<Invoice>())
+//
+//
+//        menuItemViewModel!!.getAllItems().observe(this, Observer {
+//            mAdapter!!.addAll(it ?: emptyList())
+//        })
+//
+//    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         //binding.itemImage.setImageResource(requireArguments().getInt(ARG_IMAGE_ID))
