699045914a4ae3019aea320b6bab8903257cf9d2
alexisRivaldo <axr7776@g.rit.edu>
2021-05-06 16:53:29 -0400

    added plates, salads, and dinner menus

---
+++ IslandWrapsAppFiles/app/src/main/java/com/simmonds/rachael/islandwrapsapp/DinnerActivity.kt
@@ -0,0 +1,16 @@
+package com.simmonds.rachael.islandwrapsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.simmonds.rachael.islandwrapsapp.databinding.ActivityMainBinding
+import com.simmonds.rachael.islandwrapsapp.databinding.ActivityWrapBinding
+
+class DinnerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWrapBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWrapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
